292db41e4c441ff1c8a66cb197c99eeb91b3cc7c
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2023-02-06 19:45:51 -0500

    api call done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [slip, setSlip] = useState({id: "", advice: ""})
+  const [loading, setLoading] = useState(false)
+
+  const url = 'https://api.adviceslip.com/advice'
+
+  const fetchSlip = async () => {
+    setLoading(true)
+    const res = await axios.get(url)
+    console.log(res.data)
+    setSlip(res.data.slip)
+    setLoading(false)
+  }
+
 
 
 
 
   return (
     <div className="App">
+      {loading ? 
+     <p>loading...</p> 
+    :
+    <>
+    <p>{slip.id}</p><p>{slip.advice}</p>
+    </>
+    }
+      <button onClick={fetchSlip}>Advice</button>
     </div>
   );
 }
